e1fbbb098ea821553728588bdaa2078a4f190426
sam1322 <sriram.pant@5gmail.com>
2023-08-23 07:54:15 +0530

    first commit

---
+++ src/main/java/com/example/demo/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import com.example.demo.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+
+    //    @Resource
+//    @Autowired
+//    private UserRepository repository;
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
